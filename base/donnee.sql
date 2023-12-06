93f90dd13b15f0254558074193a2b165c7819c1d
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2023-12-06 15:57:04 +0100

    base

---
+++ base/donnee.sql
@@ -0,0 +1,11 @@
+INSERT INTO salle(nom_salle) VALUES 
+('S1'),
+('S2')
+;
+
+INSERT INTO nombre_salle VALUES 
+('salle1',120,0,'2023-12-01'),
+('salle1',150,1,'2023-12-01'),
+('salle2',100,0,'2023-12-01'),
+('salle2',110,1,'2023-12-01')
+;
---
+++ base/sequence.sql
@@ -0,0 +1,13 @@
+CREATE SEQUENCE salle_sequence
+    INCREMENT 1
+    MINVALUE 1
+    START 1
+    CYCLE
+;
+
+CREATE SEQUENCE departement_sequence
+    INCREMENT 1
+    MINVALUE 1
+    START 1
+    CYCLE
+;
